2deb6b856bc0caeb8beee18903544e4a93297eb5
ankita_muddapur <ankita_muddapur@cms.co.in>
2020-03-12 16:15:39 +0530

    Adding files for Swapping Numbers.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SwapNumbersConsole;
+
+namespace SwapNumbersConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the 1st number: ");
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the 2nd number: ");
+            int value2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The entered 1st value is: " + value1 + "\n" + "The entered 2nd value is: " + value2+"\n");
+
+            var SwappedNumbers = SwapUtility.SwapNumbers(value1, value2);
+
+            Console.Write(  SwappedNumbers);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwapUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwapNumbersConsole
+{
+   public static class SwapUtility
+    {
+        static int tempVariable = 0;
+        public static string SwapNumbers(int a , int b)
+        {
+            tempVariable = a;
+            a = b;
+            b = tempVariable;
+
+            return "Value 1 contains: "+ a +"\n"+ " Value 2 contains: " + b;
+
+        }
+    }
+}
